7b29b685d808a41d0b72d1875198142399beea40
shivathmajshenoym <shivathmajshenoy@yahoo.com>
2022-05-18 13:56:27 +0530

    Step6: Used Search in API

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -3,4 +3,9 @@ class EventController < ApplicationController
     @events = Event.all
     render json: @events, only: [:title, :description]
   end
+
+  def search
+    @result = Event.where("title LIKE ?", params[:input])
+    render json: @result, only: [:title, :description]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/events" => "event#display"
+  get "/search/:input" => "event#search"
 end
