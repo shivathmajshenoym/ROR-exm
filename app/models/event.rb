f18246007e43f0a3458b6273df7016fcee36cfd6
shivathmajshenoym <shivathmajshenoy@yahoo.com>
2022-05-18 13:57:39 +0530

    Step7: Added Validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  validates :title, length: { minimum: 3, maximum: 10 }
+  validates :description, length: { minimum: 3, maximum: 10 }
+  validates :start_time, presence: true
 end
