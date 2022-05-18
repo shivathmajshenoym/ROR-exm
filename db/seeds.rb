562c7d90dff0cc41665c2f74424a9a769764be0c
shivathmajshenoym <shivathmajshenoy@yahoo.com>
2022-05-18 13:47:58 +0530

    Step3: Added Few records through Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Event.create([
+    {
+        title:"Event1",
+        description:"This is used to Event 1"
+    },
+
+    {
+        title:"Event2",
+        description:"This is used to Event 2",
+    },
+
+    {
+        title:"Event3",
+        description:"This is used to Event 3",
+    },
+
+    {
+        title:"Event4",
+        description:"This is used to Event 4"
+    }
+])
